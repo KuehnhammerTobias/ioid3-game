833fa3f54bef8e070ceebf3f1974ce58a56e58d7
ToKu <toku@gmail.com>
2015-09-16 20:29:33 +0200

    mint-arena SVN r.3820 - r.3826 https://github.com/zturtleman/mint-arena/commit/2dc5f69e34cdaab451d25407c4d4de699c95fb21

--- code/cgame/cg_draw.c
+++ code/cgame/cg_draw.c
@@ -1706,6 +1706,7 @@ static void CG_DrawCenterString( void ) {
 	int		y;
 	int		charHeight;
 	float	*color;
+	float	scale;
 
 	if ( !cg.cur_lc->centerPrintTime ) {
 		return;
@@ -1720,7 +1721,15 @@ static void CG_DrawCenterString( void ) {
 
 	start = cg.cur_lc->centerPrint;
 
-	charHeight = cg.cur_lc->centerPrintCharScale * 48.0f;
+	scale = cg.cur_lc->centerPrintCharScale;
+	charHeight = GIANTCHAR_HEIGHT;
+
+	if ( scale <= 0 ) {
+		scale = charHeight / 48.0f;
+	} else {
+		charHeight = 48 * scale;
+	}
+
 	y = cg.cur_lc->centerPrintY - cg.cur_lc->centerPrintLines * charHeight / 2;
 
 	while ( 1 ) {
@@ -1734,7 +1743,7 @@ static void CG_DrawCenterString( void ) {
 		}
 		linebuffer[l] = 0;
 
-		CG_DrawStringExt( SCREEN_WIDTH / 2, y, linebuffer, UI_CENTER|UI_DROPSHADOW|UI_GIANTFONT, color, cg.cur_lc->centerPrintCharScale, 0, 0 );
+		CG_DrawStringExt( SCREEN_WIDTH / 2, y, linebuffer, UI_CENTER|UI_DROPSHADOW|UI_GIANTFONT, color, scale, 0, 0 );
 		y += charHeight + 6;
 
 		while ( *start && ( *start != '\n' ) ) {
