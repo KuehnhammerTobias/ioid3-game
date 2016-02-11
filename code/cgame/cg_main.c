0332009e5065b765793f34c7b5cd6bf17c5ad79e
ToKu <toku@gmail.com>
2016-02-11 20:42:06 +0100

    mint-arena SVN r.4465 - r.4468 + fixed merge issue.

--- code/cgame/cg_main.c
+++ code/cgame/cg_main.c
@@ -2027,13 +2027,12 @@ qboolean CG_Load_Menu(char **p) {
 	while ( 1 ) {
 
 		token = COM_ParseExt(p, qtrue);
-    
-		if (Q_stricmp(token, "}") == 0) {
-			return qtrue;
+		if ( !token[0] ) {
+			return qfalse;
 		}
 
-		if ( !token || token[0] == 0 ) {
-			return qfalse;
+		if (Q_stricmp(token, "}") == 0) {
+			return qtrue;
 		}
 
 		CG_ParseMenu(token); 
@@ -2081,7 +2080,7 @@ void CG_LoadMenus(const char *menuFile) {
 
 	while ( 1 ) {
 		token = COM_ParseExt( &p, qtrue );
-		if( !token || token[0] == 0 || token[0] == '}') {
+		if ( !token[0] ) {
 			break;
 		}
 
