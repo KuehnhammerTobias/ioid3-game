f9c2317d3ae47f033e700a43c5ff080655eb0a1d
ToKu <toku@gmail.com>
2016-04-09 14:52:44 +0200

    AI (characteristic): new individual bot FOV. The new characteristic FOV, to enhance the bots individuality, makes more sense with monsters we'll have in COOP mode. With existing Arena Gladiators the individual fov probably will only be useful for 'Sorlag' and 'Orbb' bot (and a few custom models).
    NOTE: Won't compile and work because new characteristic isn't added to chars.h yet.

--- code/game/ai_dmq3.c
+++ code/game/ai_dmq3.c
@@ -3407,7 +3407,7 @@ int BotFindEnemy(bot_state_t *bs, int curenemy) {
 		if (curenemy < 0 && (healthdecrease || EntityIsShooting(&entinfo)))
 			f = 360;
 		else
-			f = 160;
+			f = Characteristic_BInteger(bs->character, CHARACTERISTIC_FOV, 0, 360);
 		//check if the enemy is visible
 		vis = BotEntityVisible(bs->entitynum, bs->eye, bs->viewangles, f, i);
 		if (vis <= 0) continue;
