924d279201ca14c746cf7e6955534b59210057b2
ToKu <toku@gmail.com>
2016-04-10 18:15:19 +0200

    AI (characteristic): added FOV characteristic to chars.h. Code should compile again.

--- code/game/chars.h
+++ code/game/chars.h
@@ -28,120 +28,127 @@ Suite 120, Rockville, Maryland 20850 USA.
 ===========================================================================
 */
 
-//========================================================
-//========================================================
-//name
-#define CHARACTERISTIC_NAME							0	//string
-//gender of the bot
-#define CHARACTERISTIC_GENDER						1	//string ("male", "female", "it")
-//attack skill
-// >  0.0 && <  0.2 = don't move
-// >  0.3 && <  1.0 = aim at enemy during retreat
-// >  0.0 && <  0.4 = only move forward/backward
-// >= 0.4 && <  1.0 = circle strafing
-// >  0.7 && <  1.0 = random strafe direction change
-#define CHARACTERISTIC_ATTACK_SKILL					2	//float [0, 1]
-//weapon weight file
-#define CHARACTERISTIC_WEAPONWEIGHTS				3	//string
-//view angle difference to angle change factor
-#define CHARACTERISTIC_VIEW_FACTOR					4	//float <0, 1]
-//maximum view angle change
-#define CHARACTERISTIC_VIEW_MAXCHANGE				5	//float [1, 360]
-//reaction time in seconds
-#define CHARACTERISTIC_REACTIONTIME					6	//float [0, 5]
-//accuracy when aiming
-#define CHARACTERISTIC_AIM_ACCURACY					7	//float [0, 1]
-//weapon specific aim accuracy
-#define CHARACTERISTIC_AIM_ACCURACY_MACHINEGUN		8	//float [0, 1]
-#define CHARACTERISTIC_AIM_ACCURACY_SHOTGUN			9	//float [0, 1]
-#define CHARACTERISTIC_AIM_ACCURACY_ROCKETLAUNCHER	10	//float [0, 1]
-#define CHARACTERISTIC_AIM_ACCURACY_GRENADELAUNCHER	11	//float [0, 1]
-#define CHARACTERISTIC_AIM_ACCURACY_LIGHTNING		12
-#define CHARACTERISTIC_AIM_ACCURACY_PLASMAGUN		13	//float [0, 1]
-#define CHARACTERISTIC_AIM_ACCURACY_RAILGUN			14
-#define CHARACTERISTIC_AIM_ACCURACY_BFG10K			15	//float [0, 1]
-//skill when aiming
-// >  0.0 && <  0.9 = aim is affected by enemy movement
-// >  0.4 && <= 0.8 = enemy linear leading
-// >  0.8 && <= 1.0 = enemy exact movement leading
-// >  0.5 && <= 1.0 = prediction shots when enemy is not visible
-// >  0.6 && <= 1.0 = splash damage by shooting nearby geometry
-#define CHARACTERISTIC_AIM_SKILL					16	//float [0, 1]
-//weapon specific aim skill
-#define CHARACTERISTIC_AIM_SKILL_ROCKETLAUNCHER		17	//float [0, 1]
-#define CHARACTERISTIC_AIM_SKILL_GRENADELAUNCHER	18	//float [0, 1]
-#define CHARACTERISTIC_AIM_SKILL_PLASMAGUN			19	//float [0, 1]
-#define CHARACTERISTIC_AIM_SKILL_BFG10K				20	//float [0, 1]
-//========================================================
-//chat
-//========================================================
-//file with chats
-#define CHARACTERISTIC_CHAT_FILE					21	//string
-//name of the chat character
-#define CHARACTERISTIC_CHAT_NAME					22	//string
-//characters per minute type speed
-#define CHARACTERISTIC_CHAT_CPM						23	//integer [1, 4000]
-//tendency to insult/praise
-#define CHARACTERISTIC_CHAT_INSULT					24	//float [0, 1]
-//tendency to chat misc
-#define CHARACTERISTIC_CHAT_MISC					25	//float [0, 1]
-//tendency to chat at start or end of level
-#define CHARACTERISTIC_CHAT_STARTENDLEVEL			26	//float [0, 1]
-//tendency to chat entering or exiting the game
-#define CHARACTERISTIC_CHAT_ENTEREXITGAME			27	//float [0, 1]
-//tendency to chat when killed someone
-#define CHARACTERISTIC_CHAT_KILL					28	//float [0, 1]
-//tendency to chat when died
-#define CHARACTERISTIC_CHAT_DEATH					29	//float [0, 1]
-//tendency to chat when enemy suicides
-#define CHARACTERISTIC_CHAT_ENEMYSUICIDE			30	//float [0, 1]
-//tendency to chat when hit while talking
-#define CHARACTERISTIC_CHAT_HITTALKING				31	//float [0, 1]
-//tendency to chat when bot was hit but didn't dye
-#define CHARACTERISTIC_CHAT_HITNODEATH				32	//float [0, 1]
-//tendency to chat when bot hit the enemy but enemy didn't dye
-#define CHARACTERISTIC_CHAT_HITNOKILL				33	//float [0, 1]
-//tendency to randomly chat
-#define CHARACTERISTIC_CHAT_RANDOM					34	//float [0, 1]
-//tendency to reply
-#define CHARACTERISTIC_CHAT_REPLY					35	//float [0, 1]
-//========================================================
-//movement
-//========================================================
-//tendency to crouch
-#define CHARACTERISTIC_CROUCHER						36	//float [0, 1]
-//tendency to jump
-#define CHARACTERISTIC_JUMPER						37	//float [0, 1]
+//=========================================================================
+// unless specified, the higher the number, the better the characteristic,
+// or the greater the likelyhood of the characteristic occuring.
+// also, if any characteristic is ommited, the default value will be used
+// from the file in /bots/default_c.c
+//=========================================================================
+// item weight file
+#define CHARACTERISTIC_ITEMWEIGHTS					 0 // string
+// weapon weight file
+#define CHARACTERISTIC_WEAPONWEIGHTS				 1 // string
+// file with chats
+#define CHARACTERISTIC_CHAT_FILE					 2 // string
+// name of the character with respect to the name that appears when the bot chats
+#define CHARACTERISTIC_CHAT_NAME					 3 // string
+// gender of the bot
+#define CHARACTERISTIC_GENDER						 4 // string ("male", "female", "it")
+//=========================================================================
+// battle sense
+//=========================================================================
+// reaction time in seconds, the lower the number the better
+#define CHARACTERISTIC_REACTIONTIME					 5 // float [0, 5]
+// how alert the bot is
+#define CHARACTERISTIC_ALERTNESS					 6 // float [0, 1]
+// the field of view of the bot
+#define CHARACTERISTIC_FOV							 7 // float [0, 360]
+// view angle difference to angle change factor
+#define CHARACTERISTIC_VIEW_FACTOR					 8 // float [0, 1]
+// maximum view angle change
+#define CHARACTERISTIC_VIEW_MAXCHANGE				 9 // float [1, 360]
+// the aggression of the bot
+// > 0.0 && <  0.2 = don't shoot at dead bodies
+// > 0.0 && <= 0.9 = retreat if feeling bad
+// > 0.9 && <= 1.0 = don't retreat even if low on health
+#define CHARACTERISTIC_AGGRESSION					10 // float [0, 1]
+// the self preservation of the bot (rockets near walls etc.)
+#define CHARACTERISTIC_SELFPRESERVATION				11 // float [0, 1]
+// how likely the bot is to take revenge
+#define CHARACTERISTIC_VENGEFULNESS					12 // float [0, 1]
+// tendency to get easy frags (such as on chatting players)
+#define CHARACTERISTIC_EASY_FRAGGER					13 // float [0, 1]
+// tendency to camp
+#define CHARACTERISTIC_CAMPER						14 // float [0, 1]
+//=========================================================================
+// movement
+//=========================================================================
+// attack skill
+// >  0.0 && < 0.2 = don't move
+// >  0.3 && < 1.0 = aim at enemy during retreat
+// >  0.0 && < 0.4 = only move forward/backward
+// >= 0.4 && < 1.0 = circle strafing
+// >  0.7 && < 1.0 = random strafe direction change
+#define CHARACTERISTIC_ATTACK_SKILL					15 // float [0, 1]
+// tendency to crouch
+#define CHARACTERISTIC_CROUCHER						16 // float [0, 1]
+// tendency to jump
+#define CHARACTERISTIC_JUMPER						17 // float [0, 1]
 //tendency to walk
-#define CHARACTERISTIC_WALKER						48	//float [0, 1]
-//tendency to jump using a weapon
-#define CHARACTERISTIC_WEAPONJUMPING				38	//float [0, 1]
-//tendency to use the grapple hook when available
-#define CHARACTERISTIC_GRAPPLE_USER					39	//float [0, 1]	//use this!!
-//========================================================
-//goal
-//========================================================
-//item weight file
-#define CHARACTERISTIC_ITEMWEIGHTS					40	//string
-//the aggression of the bot
-#define CHARACTERISTIC_AGGRESSION					41	//float [0, 1]
-//the self preservation of the bot (rockets near walls etc.)
-#define CHARACTERISTIC_SELFPRESERVATION				42	//float [0, 1]
-//how likely the bot is to take revenge
-#define CHARACTERISTIC_VENGEFULNESS					43	//float [0, 1]
-//tendency to camp
-#define CHARACTERISTIC_CAMPER						44	//float [0, 1]
-//========================================================
-//========================================================
-//tendency to get easy frags
-#define CHARACTERISTIC_EASY_FRAGGER					45	//float [0, 1]
-//how alert the bot is (view distance)
-#define CHARACTERISTIC_ALERTNESS					46	//float [0, 1]
-//how much the bot fires its weapon
-#define CHARACTERISTIC_FIRETHROTTLE					47	//float [0, 1]
-//========================================================
-//========================================================
-//note: 48 is CHARACTERISTIC_WALKER
-//max characteristics for game VM
-#define MAX_CHARACTERISTICS							49
-
+#define CHARACTERISTIC_WALKER						18	//float [0, 1]
+// tendency to jump using a weapon
+#define CHARACTERISTIC_WEAPONJUMPING				19 // float [0, 1]
+//=========================================================================
+// weapon
+//=========================================================================
+// skill when aiming
+// > 0.0 && <  0.9 = aim is affected by enemy movement
+// > 0.4 && <= 0.8 = enemy linear leading
+// > 0.8 && <= 1.0 = enemy exact movement leading
+// > 0.5 && <= 1.0 = prediction shots when enemy is not visible
+// > 0.6 && <= 1.0 = splash damage by shooting nearby geometry
+#define CHARACTERISTIC_AIM_SKILL					20 // float [0, 1]
+// weapon specific aim skill
+#define CHARACTERISTIC_AIM_SKILL_NAILGUN			21 // float [0, 1]
+#define CHARACTERISTIC_AIM_SKILL_GRENADELAUNCHER	22 // float [0, 1]
+#define CHARACTERISTIC_AIM_SKILL_ROCKETLAUNCHER		23 // float [0, 1]
+#define CHARACTERISTIC_AIM_SKILL_PLASMAGUN			24 // float [0, 1]
+#define CHARACTERISTIC_AIM_SKILL_BFG10K				25 // float [0, 1]
+// accuracy when aiming
+#define CHARACTERISTIC_AIM_ACCURACY					26 // float [0, 1]
+// weapon specific aim accuracy
+#define CHARACTERISTIC_AIM_ACCURACY_MACHINEGUN		27 // float [0, 1]
+#define CHARACTERISTIC_AIM_ACCURACY_CHAINGUN		28 // float [0, 1]
+#define CHARACTERISTIC_AIM_ACCURACY_SHOTGUN			29 // float [0, 1]
+#define CHARACTERISTIC_AIM_ACCURACY_NAILGUN			30 // float [0, 1]
+#define CHARACTERISTIC_AIM_ACCURACY_PROXLAUNCHER	31 // float [0, 1]
+#define CHARACTERISTIC_AIM_ACCURACY_GRENADELAUNCHER	32 // float [0, 1]
+#define CHARACTERISTIC_AIM_ACCURACY_ROCKETLAUNCHER	33 // float [0, 1]
+#define CHARACTERISTIC_AIM_ACCURACY_LIGHTNING		34 // float [0, 1]
+#define CHARACTERISTIC_AIM_ACCURACY_RAILGUN			35 // float [0, 1]
+#define CHARACTERISTIC_AIM_ACCURACY_PLASMAGUN		36 // float [0, 1]
+#define CHARACTERISTIC_AIM_ACCURACY_BFG10K			37 // float [0, 1]
+// how much the bot fires its weapon (continuous or bursts)
+#define CHARACTERISTIC_FIRETHROTTLE					38 // float [0, 1]
+//=========================================================================
+// chat
+//=========================================================================
+// tendency to insult/praise
+#define CHARACTERISTIC_CHAT_INSULT					39 // float [0, 1]
+// tendency to chat about miscellaneous subject
+#define CHARACTERISTIC_CHAT_MISC					40 // float [0, 1]
+// tendency to chat at start or end of level
+#define CHARACTERISTIC_CHAT_STARTENDLEVEL			41 // float [0, 1]
+// tendency to chat entering or exiting the game
+#define CHARACTERISTIC_CHAT_ENTEREXITGAME			42 // float [0, 1]
+// tendency to chat when the character killed someone
+#define CHARACTERISTIC_CHAT_KILL					43 // float [0, 1]
+// tendency to chat when the character died
+#define CHARACTERISTIC_CHAT_DEATH					44 // float [0, 1]
+// tendency to chat when enemy commits suicide
+#define CHARACTERISTIC_CHAT_ENEMYSUICIDE			45 // float [0, 1]
+// tendency to chat when hit while chatting
+#define CHARACTERISTIC_CHAT_HITTALKING				46 // float [0, 1]
+// tendency to chat when bot was hit but didn't die
+#define CHARACTERISTIC_CHAT_HITNODEATH				47 // float [0, 1]
+// tendency to chat when bot hit the enemy but enemy didn't die
+#define CHARACTERISTIC_CHAT_HITNOKILL				48 // float [0, 1]
+// tendency to chat at random moments
+#define CHARACTERISTIC_CHAT_RANDOM					59 // float [0, 1]
+// tendency to reply to chats
+#define CHARACTERISTIC_CHAT_REPLY					50 // float [0, 1]
+// tendency to use the grapple hook when available
+#define CHARACTERISTIC_GRAPPLE_USER					51 // float [0, 1]
+//=========================================================================
+// max characteristics for game VM
+#define MAX_CHARACTERISTICS							52
